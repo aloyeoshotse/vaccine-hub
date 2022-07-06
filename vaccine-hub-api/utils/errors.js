f1c2f29470d8a2afe6cd3a540b4f015058ca6679
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-06 15:22:27 -0700

    added new errors

--- vaccine-hub-api/utils/errors.js
+++ vaccine-hub-api/utils/errors.js
@@ -6,20 +6,35 @@ class ExpressError extends Error {
     }
 }
 
-class NotFoundError extends ExpressError {
-    constructor(message = "Not Found"){
-        super(message, 404);
-    }
-}
-
 class BadRequestError extends ExpressError {
     constructor(message = "Bad Request") {
         super(message,400);
     }
 }
 
+class UnauthorizedError extends ExpressError {
+    constructor(message = "Unauthorized") {
+        super(message,401);
+    }
+}
+
+class ForbiddenError extends ExpressError {
+    constructor(message = "Forbidden") {
+        super(message,403);
+    }
+}
+
+class NotFoundError extends ExpressError {
+    constructor(message = "Not Found"){
+        super(message, 404);
+    }
+}
+
+
 module.exports = {
     ExpressError,
-    NotFoundError,
-    BadRequestError
+    BadRequestError,
+    UnauthorizedError,
+    ForbiddenError,
+    NotFoundError
 }
