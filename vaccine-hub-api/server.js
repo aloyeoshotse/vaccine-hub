8dd210821b028138462b6fd056ea9a8cecc34b19
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-06 15:06:26 -0700

    deleted app.js and added changes to server.js

--- vaccine-hub-api/server.js
+++ vaccine-hub-api/server.js
@@ -1,7 +1,16 @@
-const app = require("./app");
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const server = express()
+
+server.use(morgan('tiny'));
+server.use(express.json());
+server.use(cors());
+
 
 const PORT = process.env.port || 3001
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`🚀 Server listening on http://localhost:${PORT}`)
 });
