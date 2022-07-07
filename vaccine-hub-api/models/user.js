ac201ef4c0d3a5c083211bf0e356086596ebbe14
Aloye Oshotse <aloye.oshotse@gmail.com>
2022-07-06 23:21:47 -0700

    updated user model

--- vaccine-hub-api/models/user.js
+++ vaccine-hub-api/models/user.js
@@ -1,5 +1,5 @@
 const db = require('../db')
-const {UnauthorizedError} = require('../utils/errors');
+const {UnauthorizedError, BadRequestError} = require('../utils/errors');
 
 class User {
     static async login(credentials) {
@@ -16,18 +16,62 @@ class User {
     }
 
     static async register(credentials) {
-        /* user should submit their email, pw, rsvp status, and # of guests
-           if any of these fields are missing, throw an error
+        /* user should submit their email, pw, first name, last name, location, and date
+           if any of these fields are missing, throw an error */
+        const requiredFields = ['password', 'firstName', 'lastName', 'email', 'location']
+        requiredFields.forEach(field => {
+            if (!credentials.hasOwnProperty(field)) {
+                throw new BadRequestError(`Missing ${field} in request body.`);
+            }
+        })
+
+        if (credentials.email.indexOf("@") <= 0) {
+            throw new BadRequestError("Invalid email.")
+        }
            
-           make sure no user already exists in the system with that email
-           if one does, throw an error
+        /* make sure no user already exists in the system with that email
+           if one does, throw an error */
+        const existingUser = await User.fetchUserByEmail(credentials.email)
+        if (existingUser) {
+            throw new BadRequestError(`Duplicate email: ${credentials.email}`)
+        }
 
-           take the user's password, and hash it
-           take the user's email, and lowercase it
+        /*take the user's password, and hash it
+           take the user's email, and lowercase it */
+        const lowercasedEmail = credentials.email.toLowerCase();
 
-           create a new user in the database with all their info
+        /* create a new user in the database with all their info
            return the user
             */
+        const result = await db.query(`
+        INSERT INTO users (
+            password,
+            first_name,
+            last_name,
+            email,
+            location
+        )
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING id, password, first_name, last_name, email, location, date;
+        `, [credentials.password, credentials.firstName, credentials.lastName, lowercasedEmail, credentials.location])
+
+        const user = result.rows[0]
+
+        return user;
+    }
+
+    static async fetchUserByEmail(email) {
+        if (!email) {
+            throw new BadRequestError('No email provided.')
+        }
+
+        const query = `SELECT * FROM users WHERE email = $1`
+
+        const result = await db.query(query, [email.toLowerCase()])
+
+        const user = result.rows[0]
+
+        return user
     }
 }
 
--- vaccine-hub-api/routes/auth.js
+++ vaccine-hub-api/routes/auth.js
@@ -5,7 +5,8 @@ const router = express.Router()
 router.post('/login', async(req,res, next) => {
     try{
         //take email and passwords and attempt to authenticate
-
+        const user = await User.login(req.body);
+        return res.status(200).json({ user });
     }
     catch(err) {
         next(err)
@@ -16,6 +17,8 @@ router.post('/register', async(req,res, next) => {
     try{
         /*take user email, password, rsvp status, and number of guests 
             and creat a new user in database */
+        const user = await User.register(req.body);
+        return res.status(201).json({ user });
     }
     catch(err) {
         next(err)
